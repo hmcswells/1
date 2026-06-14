6d508fd3a018473e2647f6f68f4caee73dd5ccca
scott wells <hmcswells@gmail.com>
2026-06-14 15:40:12 -0400

    Fix syntax and move basename to Router component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter basename="/1" as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
@@ -58,15 +58,16 @@ const AuthenticatedApp = () => {
 function App() {
 
   return (
+<Router basename="/1">
+  <QueryClientProvider client={queryClientInstance}>
     <AuthProvider>
-      <QueryClientProvider client={queryClientInstance}>
-        <Router>
-          <ScrollToTop />
-          <AuthenticatedApp />
-        </Router>
-        <Toaster />
-      </QueryClientProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </AuthProvider>
+  </QueryClientProvider>
+</Router>
   )
 }
 
