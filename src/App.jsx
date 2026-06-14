3b378c7956d1ba88621af63c191a37ead27eea5d
scott wells <hmcswells@gmail.com>
2026-06-14 17:03:12 -0400

    Fix GitHub Pages routing and image bundle

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
@@ -58,16 +58,14 @@ const AuthenticatedApp = () => {
 function App() {
 
   return (
-<Router basename="/1">
-  <QueryClientProvider client={queryClientInstance}>
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </AuthProvider>
-  </QueryClientProvider>
-</Router>
+    <Router>
+      <QueryClientProvider client={queryClientInstance}>
+        <AuthProvider>
+          <AuthenticatedApp />
+          <Toaster />
+        </AuthProvider>
+      </QueryClientProvider>
+    </Router>
   )
 }
 
